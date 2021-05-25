963ffedd0f7dc205e78710c20ac612ad8c0acf9c
Tatiana <tatiana.rodriguez@tecnicall.com.co>
2021-05-25 06:35:06 -0500

    Open graph SEO cleaning code

--- src/components/templates/SEO.tsx
+++ src/components/templates/SEO.tsx
@@ -1,69 +1,41 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import background from '../../assets/img/bg-people.@2x.png'
+import background from "../../assets/img/bg-people.@2x.png";
 function SEO() {
   return (
     <Helmet>
       <title>Rule Of Thumb</title>
-      <meta name="description" content="Nested component" />
       <meta
-        data-react-helmet="true"
-        name="twitter:card"
-        content="summary_large_image"
+        name="description"
+        content="Tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc."
       />
-      <meta
-        data-react-helmet="true"
-        name="twitter:site"
-        content="@trodriguez30"
-      />
-      <meta
-        name="twitter:creator"
-        content="Tatiana Rodríguez"
-        data-react-helmet="true"
-      />
-      <meta
-        name="twitter:title"
-        content="Rule Of Thumbs"
-        data-react-helmet="true"
-      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@trodriguez30" />
+      <meta name="twitter:creator" content="Tatiana Rodríguez" />
+      <meta name="twitter:title" content="Rule Of Thumbs" />
       <meta
         name="twitter:description"
         content="Tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc."
-        data-react-helmet="true"
       />
-      <meta
-        name="twitter:image"
-        content={background}
-        data-react-helmet="true"
-      />
+      <meta name="twitter:image" content={background} />
       ​
-      <meta property="og:site_name" content="Rule Of Thumbs" data-react-helmet="true" />
       <meta
-        property="og:title"
-        content="Rule Of Thumb"
+        property="og:site_name"
+        content="Rule Of Thumbs"
         data-react-helmet="true"
       />
+      <meta property="og:title" content="Rule Of Thumb" />
       <meta
         property="og:url"
         content="https://rule-of-thumb-ui-test.vercel.app/"
-        data-react-helmet="true"
       />
       <meta
         property="og:description"
         content="tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc."
-        data-react-helmet="true"
       />
-      <meta
-        property="og:image"
-        content={background}
-        data-react-helmet="true"
-      />
-      <meta
-        property="og:image:alt"
-        content="Rule Of Thum Background"
-        data-react-helmet="true"
-      />
-      <meta property="og:type" content="website" data-react-helmet="true" />
+      <meta property="og:image" content={background} />
+      <meta property="og:image:alt" content="Rule Of Thum Background" />
+      <meta property="og:type" content="website" />
     </Helmet>
   );
 }
