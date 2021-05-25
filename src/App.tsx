075e1732328323a044d416d2f5419957efb2208d
Tatiana <tatiana.rodriguez@tecnicall.com.co>
2021-05-25 09:31:02 -0500

    added hotjar config to get heatmap from web

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,16 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/configureStore";
 
+//import SEO tools for tracking user behaviour
 import ReactGA from 'react-ga';
+import { hotjar } from 'react-hotjar';
+ 
+//Google analytics init config to track user behavior and get feedback from web
 ReactGA.initialize('UA-197871343-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
+
+//Hotjar config to obtain heat map according to user behavior
+hotjar.initialize(2419809, 6);
 
 function App() {
   return (
