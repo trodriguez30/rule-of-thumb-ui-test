587188325edaf192575184e438f6f4e025a95fb0
Tatiana <tatiana.rodriguez@tecnicall.com.co>
2021-05-25 07:48:01 -0500

    Google analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import Home from "./pages/Home";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/configureStore";
+import ReactGA from 'react-ga';
 
+ReactGA.initialize(`${process.env.GOOGLE_ANALYTICS_ID_TRACKING}`);
+ReactGA.pageview(window.location.pathname + window.location.search);
 function App() {
   return (
     <Provider store={store}>
