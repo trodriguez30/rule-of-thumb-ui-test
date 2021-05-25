d6e9356c62246ef6f85b4eb0b1e3242a3ba81ab9
Tatiana <tatiana.rodriguez@tecnicall.com.co>
2021-05-25 06:48:05 -0500

    Open graph SEO refactoring

--- src/components/templates/SEO.tsx
+++ src/components/templates/SEO.tsx
@@ -3,40 +3,55 @@ import { Helmet } from "react-helmet";
 import background from "../../assets/img/bg-people.@2x.png";
 function SEO() {
   return (
-    <Helmet>
-      <title>Rule Of Thumb</title>
-      <meta
-        name="description"
-        content="Tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc."
-      />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@trodriguez30" />
-      <meta name="twitter:creator" content="Tatiana Rodríguez" />
-      <meta name="twitter:title" content="Rule Of Thumbs" />
-      <meta
-        name="twitter:description"
-        content="Tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc."
-      />
-      <meta name="twitter:image" content={background} />
-      ​
-      <meta
-        property="og:site_name"
-        content="Rule Of Thumbs"
-        data-react-helmet="true"
-      />
-      <meta property="og:title" content="Rule Of Thumb" />
-      <meta
-        property="og:url"
-        content="https://rule-of-thumb-ui-test.vercel.app/"
-      />
-      <meta
-        property="og:description"
-        content="tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc."
-      />
-      <meta property="og:image" content={background} />
-      <meta property="og:image:alt" content="Rule Of Thum Background" />
-      <meta property="og:type" content="website" />
-    </Helmet>
+    <Helmet
+      htmlAttributes={{ lang: "es" }}
+      title="Rule Of Thumbs"
+      meta={[
+        {
+          name: "description",
+          content:
+            "Tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc.",
+        },
+        {
+          name: "keywords",
+          content:
+            "controversial, people, trending, politics, business, media, entertainment",
+        },
+        {
+          property: "og:url",
+          content: "https://rule-of-thumb-ui-test.vercel.app/",
+        },
+        {
+          property: "og:title",
+          content: "Rule Of Thumb",
+        },
+        {
+          name: "og:description",
+          content:
+            "Tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc.",
+        },
+        {
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        {
+          name: "twitter:creator",
+          content: "@trodriguez30",
+        },
+        {
+          name: "twitter:title",
+          content: "Rule Of Thumbs",
+        },
+        {
+          name: "twitter:description",
+          content:
+            "Tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc.",
+        },
+      ].concat([
+        { property: "og:image", content: background },
+        { name: "twitter:image", content: background },
+      ])}
+    />
   );
 }
 
