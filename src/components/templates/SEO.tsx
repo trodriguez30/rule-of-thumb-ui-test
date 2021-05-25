271e1417cf8f189a37080b4e0a8cf8bd88d8f615
Tatiana <tatiana.rodriguez@tecnicall.com.co>
2021-05-25 06:56:15 -0500

    Open graph SEO refactoring

--- src/components/templates/SEO.tsx
+++ src/components/templates/SEO.tsx
@@ -1,57 +1,36 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import background from "../../assets/img/bg-people.@2x.png";
+const HOST_URL = 'https://rule-of-thumb-ui-test.vercel.app/'
 function SEO() {
   return (
-    <Helmet
-      htmlAttributes={{ lang: "es" }}
-      title="Rule Of Thumbs"
-      meta={[
-        {
-          name: "description",
-          content:
-            "Tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc.",
-        },
-        {
-          name: "keywords",
-          content:
-            "controversial, people, trending, politics, business, media, entertainment",
-        },
-        {
-          property: "og:url",
-          content: "https://rule-of-thumb-ui-test.vercel.app/",
-        },
-        {
-          property: "og:title",
-          content: "Rule Of Thumb",
-        },
-        {
-          name: "og:description",
-          content:
-            "Tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc.",
-        },
-        {
-          name: "twitter:card",
-          content: "summary_large_image",
-        },
-        {
-          name: "twitter:creator",
-          content: "@trodriguez30",
-        },
-        {
-          name: "twitter:title",
-          content: "Rule Of Thumbs",
-        },
-        {
-          name: "twitter:description",
-          content:
-            "Tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc.",
-        },
-      ].concat([
-        { property: "og:image", content: background },
-        { name: "twitter:image", content: background },
-      ])}
-    />
+    <Helmet>
+      <title>Rule Of Thumbs</title>
+      <meta name="description" content="Tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc. " />
+      <meta
+        property="og:title"
+        content="Rule Of Thumbs"
+      />
+      <meta property="og:description" content="Tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc. " />
+      <meta
+        property="og:image"
+        itemProp="image"
+        content={`${HOST_URL}/${background}`}
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={HOST_URL} />
+      <meta
+        property="twitter:title"
+        content="Rule Of Thumb"
+      />
+      <meta property="twitter:description" content="Tracks the sentiment of their users on trending and controversial people from different fields, including politics, business, media and entertainment, etc. " />
+      <meta
+        property="twitter:image"
+        itemProp="image"
+        content={`${HOST_URL}/${background}`}
+      />
+      <meta property="twitter:card" content="summary_large_image" />
+    </Helmet>
   );
 }
 
